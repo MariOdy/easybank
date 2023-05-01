9b11db302acda7a077e2870eeb558a8b3718be86
Marii Odynska <mvasylenko21@gmail.com>
2023-05-01 13:19:57 +0300

    Add Buttons component

---
+++ components/Buttons/RequestInvite.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const RequestInvite: React.FC = () => {
+  return <button className={styles.request_btn}>Request Invite</button>;
+};
+
+export default RequestInvite;
